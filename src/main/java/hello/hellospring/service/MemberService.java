2aee5032ff6db42c08b2e83d4b5c521b3840bc96
keh6305 <keh6305@gmail.com>
2022-05-11 00:35:26 +0900

    서비스 및 테스트 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,5 +1,55 @@
 package hello.hellospring.service;
 
-public interface MemberService {
+import java.util.List;
+import java.util.Optional;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+public class MemberService
+{
+	private final MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository)
+	{
+		this.memberRepository = memberRepository;
+	}
+	
+	// 회원가입
+	public Long join(Member member)
+	{
+		// 중복 회원 검사
+		validateDuplicateMember(member);
+		
+		memberRepository.save(member);
+		
+		return member.getId();
+	}
+	
+	// 같은 이름 중복 확인
+	private void validateDuplicateMember(Member member)
+	{
+//		Optional<Member> result = memberRepository.findByName(member.getName());
+//		
+//		result.ifPresent(m -> {
+//			throw new IllegalStateException("이미 존재하는 회원입니다.");
+//		});
+		
+		memberRepository.findByName(member.getName())
+			.ifPresent(m -> {
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
+	}
+	
+	// 전체 회원 조회
+	public List<Member> findMembers()
+	{
+		return memberRepository.findAll();
+	}
+	
+	// 단일 회원 조회
+	public Optional<Member> findOne(Long memgerId)
+	{
+		return memberRepository.findById(memgerId);
+	}
 }
