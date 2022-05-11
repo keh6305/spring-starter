d16fcd60ad709574acd096e42ff20e2e102b4e0d
Eunho_Kim <keh6305@gmail.com>
2022-05-12 00:41:25 +0900

    MVC 모델 간단 구현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -14,5 +20,32 @@ public class MemberController
 	public MemberController(MemberService memberService)
 	{
 		this.memberService = memberService;
+	}
+	
+	@GetMapping("/member/new")
+	public String createMemberForm()
+	{
+		return "member/createMemberForm";
+	}
+	
+	@PostMapping("/member/new")
+	public String createMember(MemberForm form)
+	{
+		Member member = new Member();
+		member.setName(form.getName());
+		
+		memberService.join(member);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/members")
+	public String List(Model model)
+	{
+		List<Member> members = memberService.findMembers();
+		
+		model.addAttribute("members", members);
+		
+		return "member/memberList";
 	}
 }
